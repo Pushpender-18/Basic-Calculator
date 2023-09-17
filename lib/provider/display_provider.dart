30f958b540911aaacbfe29a34efc4f3203d8aae4
Pushpender <pushpender272320@gmail.com>
2023-09-17 23:40:25 +0530

    Removed consequtive operators glitch

--- lib/provider/display_provider.dart
+++ lib/provider/display_provider.dart
@@ -1,10 +1,19 @@
+import 'package:calculator/widgets/keyboard.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FocusedText extends StateNotifier<String> {
   FocusedText() : super('');
 
   void addText(String text) {
-    state = state + text;
+    if (state.length < 16) {
+      if (state.isNotEmpty) {
+        if (operators.keys.contains(state[state.length - 1]) &&
+            operators.keys.contains(text)) {
+          state = state.substring(0, state.length - 1);
+        }
+      }
+      state = state + text;
+    }
   }
 
   void clearText() {
@@ -12,7 +21,9 @@ class FocusedText extends StateNotifier<String> {
   }
 
   void deleteText() {
-    state = state.substring(0, state.length - 1);
+    if (state.isNotEmpty) {
+      state = state.substring(0, state.length - 1);
+    }
   }
 }
 
