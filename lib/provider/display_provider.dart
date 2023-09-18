8759753e306f7416dba548f0e3aee6ef964f922d
Pushpender <pushpender272320@gmail.com>
2023-09-18 22:05:18 +0530

    Fixed multiple decimals and illegal operator bug

--- lib/provider/display_provider.dart
+++ lib/provider/display_provider.dart
@@ -6,10 +6,15 @@ class FocusedText extends StateNotifier<String> {
 
   void addText(String text) {
     if (state.length < 16) {
-      if (state.isNotEmpty) {
+      if (state.isEmpty && operators.keys.contains(text)) {
+        text = '';
+      } else if (state.isNotEmpty) {
         if (operators.keys.contains(state[state.length - 1]) &&
             operators.keys.contains(text)) {
           state = state.substring(0, state.length - 1);
+        } else if (state.substring(state.length - 1, state.length) == '.' &&
+            text == '.') {
+          text = '';
         }
       }
       state = state + text;
